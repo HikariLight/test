985003533d2c079c0b62ab165f77db3420df78e3
Dhia M <merzougui.dhia@gmail.com>
2024-06-09 21:03:44 +0200

    Updated error handling

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import "./styles/index.css"
 import { WatchModel, WatchView, WatchController } from "./ts"
 
 document.addEventListener("DOMContentLoaded", () => {
-    const watchModel = WatchModel.getInstance()
-    const controller = new WatchController(watchModel)
-    const watchView = new WatchView("root", controller)
-
-    watchModel.attach(watchView)
-
-    controller.createWatch("Europe/Paris")
-
-    // Rendering
     try {
+        const watchModel = WatchModel.getInstance()
+        const controller = new WatchController(watchModel)
+        const watchView = new WatchView("root", controller)
+
+        watchModel.attach(watchView)
+
+        controller.createWatch("Europe/Paris")
+
+        // Rendering
         watchView.render()
         const intervalId = setInterval(() => watchView.render(), 1000) // Re-render every 1s
     } catch (error) {
